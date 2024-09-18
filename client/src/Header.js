cbd30f1f96d646fe94274e8d337f8c1c8839d046
aditya110802 <aditya110802verma@gmail.com>
2024-09-18 18:11:51 +0530

    files upload

---
+++ client/src/Header.js
@@ -0,0 +1,51 @@
+import { Link, useLocation } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  const location = useLocation(); // Get the current route
+
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  // Conditionally render logout only if not on PostPage (e.g., /post/:id)
+  const isPostPage = location.pathname.startsWith("/post");
+
+  return (
+    <header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            {!isPostPage && <a onClick={logout}>Logout</a>} {/* Hide logout on PostPage */}
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
